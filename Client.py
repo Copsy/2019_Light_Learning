0884f28870b477608d2dae1cdbfff736697b7d43
asd0755 <39663810+asd0755@users.noreply.github.com>
2019-11-28 15:47:11 +0900

    Update Client.py

--- Client.py
+++ Client.py
@@ -24,18 +24,24 @@ cap=cv.VideoCapture(pipeline(framerate=60), cv.CAP_GSTREAMER)
 end_msg=str(-1)
 
 while True:
+    # 카메라 프레임 읽어 오기
     ref,frame=cap.read()
     
     cv.rectangle(frame,(x_1-5,y_1-5),(x_2+5,y_2+5),(255,255,255),3)
-    
+    # 프레임 보여주기
     cv.imshow("Figure_1", frame)
+    # 버튼의 입력을 기다림 30ms
     key=cv.waitKey(30)
-    if key==32:
+    if key==32: #Space bar --> Take a sanpshot
         ROI=(frame.copy())[x_1:x_2,y_1:y_2]
+        #Encode
         encode_param=[int(cv.IMWRITE_JPEG_QUALITY),90]
+        
         result, encode=cv.imencode(".jpg", ROI, encode_param)
         data=np.array(encode)
         strData=data.tostring()
+        #Encode is done
+        
         #Send Image
         client_sock.send(str(len(strData)).ljust(16).encode())
         client_sock.send(strData)
@@ -44,11 +50,13 @@ while True:
             msg=client_sock.recv(SIZE)
         except Exception as e:
             _=0;
+        # CV 이용 새로운 화면 띄우고 Display
         print("Result is "+msg.decode())
-    elif key==27:
+    elif key==27: # 종료 버튼
+        # 서버에게 종료를 알림
         client_sock.send(end_msg.encode())
         break;
-
+#소켓 닫기
 client_sock.close()
 cap.release()
 cv.destroyAllWindows()
