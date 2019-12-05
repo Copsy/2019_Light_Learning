9a4490bacd7c5b92dd05b097dc26247f615188ac
Copsy <52805274+Copsy@users.noreply.github.com>
2019-12-05 16:59:31 +0900

    1205

--- Client.py
+++ Client.py
@@ -5,55 +5,81 @@ Created on Wed Nov 27 02:12:05 2019
 @author: Alero
 """
 
+WHITE=(255,255,255)
+display_width=1000
+display_height=200
+window_size=(display_width,display_height)
+
+TCP_IP="192.168.0.2"
+TCP_PORT=12345
+
 import socket
 import cv2 as cv
 import numpy as np
 from camera_pipeline import pipeline
+from Voice import speak
 
-TCP_IP="192.168.0.2"
-TCP_PORT=5678
+font=cv.FONT_HERSHEY_PLAIN
+fontscale=5
+font_location=(90,90)
+zero=np.zeros((display_height,display_width), dtype=np.uint8)
 x_1,y_1,x_2,y_2=100,100,340,340
-SIZE=8
-client_sock=socket.socket()
+SIZE=16
+
+client_sock=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 client_sock.connect((TCP_IP,TCP_PORT))
+#cap=cv.VideoCapture(pipeline(framerate=60),cv.CAP_GSTREAM)
 cap=cv.VideoCapture(0)
+
+word=""
 
 end_msg=str(-1)
 
 while True:
-    # 카메라 프레임 읽어 오기
     ref,frame=cap.read()
+    frame=cv.flip(frame,1)
+    cv.rectangle(frame,(x_1-5,y_1-5),(x_2+5,y_2+5),WHITE,3)
     
-    cv.rectangle(frame,(x_1-5,y_1-5),(x_2+5,y_2+5),(255,255,255),3)
-    # 프레임 보여주기
     cv.imshow("Figure_1", frame)
-    # 버튼의 입력을 기다림 30ms
+    
+    cv.namedWindow("WORD")
+    cv.moveWindow("WORD",300,550)
+    cv.resizeWindow("WORD",window_size)
+    cv.imshow("WORD",zero)
+    
     key=cv.waitKey(30)
-    if key==32: #Space bar --> Take a sanpshot
+    if key==32:
         ROI=(frame.copy())[x_1:x_2,y_1:y_2]
-        #Encode
         encode_param=[int(cv.IMWRITE_JPEG_QUALITY),90]
-        
         result, encode=cv.imencode(".jpg", ROI, encode_param)
         data=np.array(encode)
         strData=data.tostring()
-        #Encode is done
-        
         #Send Image
-        client_sock.send(str(len(strData)).ljust(16).encode())
+        client_sock.send(str(len(strData)).ljust(16).encode())#b'len'
         client_sock.send(strData)
         # Waiting for Result
         try:
             msg=client_sock.recv(SIZE)
+            word+=msg.decode()
         except Exception as e:
             _=0;
-        # CV 이용 새로운 화면 띄우고 Display
-        print("Result is "+msg.decode())
-    elif key==27: # 종료 버튼
-        # 서버에게 종료를 알림
+
+        cv.putText(zero,word,font_location,font,fontscale,WHITE,5)
+        
+    elif key==ord('a'):
+        speak(word)
+        
+    elif key==ord('s'):#erase
+        word=word[:-1]
+        
+    elif key==ord('d'):#RESET
+        word=""
+        zero=np.zeros((display_height,display_width), dtype=np.uint8)
+        
+    elif key==27:
         client_sock.send(end_msg.encode())
         break;
-#소켓 닫기
+
 client_sock.close()
 cap.release()
 cv.destroyAllWindows()
--- Server_Prediction.py
+++ Server_Prediction.py
@@ -5,50 +5,48 @@ Created on Sun Nov 24 22:19:35 2019
 @author: Alero
 """
 import numpy as np
-import os
 from keras.models import load_model
 import cv2 as cv
-#배경 추출
 from Background_Abstract import abstract
-#이미지 크기 변경 200 200 1
 from size_change import sizing
 import socket
 
-#스트링 바이트 스트림을 이미지로 변환
 def recvall(sock, count):
     key=True
     buf = b''
     while count:
-        newbuf = sock.recv(count)
+        try:
+            newbuf=sock.recv(count)
+        except Exception as e:
+            _=0
         
-        # 종료 신호의 확인
         if len(newbuf)==2:
             print("Key is False")
             key=False
             return buf,key
             
-        if not newbuf: return None
+        if not newbuf: 
+            return None
         buf += newbuf
         count -= len(newbuf)
     return buf, key 
 '''
 Need to change TCP_IP
 '''
-TCP_IP = '192.168.0.2'
-TCP_PORT = 5678
-#클라이언트의 종료 확인 flag
+TCP_IP="192.168.0.2"
+TCP_PORT = 12345
+
 ref=True
 
 X_kernel=np.array([[1,1,1],[1,1,1],[1,1,1]])
-# 저장된 모델을 불러 오기
 H5_path="./Learning_Model_V9_1.h5"
 model=load_model(H5_path)
 Label=[]
 count=0
 
-#어디 알파벳 까지 구현되었는가
 for i in range(26):
     Label.append(chr(65+i))
+
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 print("OPEN")
@@ -56,40 +54,34 @@ s.bind((TCP_IP, TCP_PORT))
 print("Bind")
 s.listen(True)
 print("Listen")
-#통신 구간
+
 while True:
     ref=True;    
     conn, addr = s.accept()
     print("Accept")
     while True:
         try:
-            #이미지 올떄까지 기다림
             length, ref=recvall(conn,16)
-            # 종료 신호 인지 이미지 인지 확인
+            
             if ref==False:
                 print("Close connection with client")
                 break;
-            # DEcode
+                
             stringData, ref=recvall(conn,int(length))
             data=np.fromstring(stringData,dtype="uint8")
-            decimg=cv.imdecode(data,1)
+            decimg=cv.imdecode(data,1)#Not defined
             
-            # 이미지 전처리
             img=cv.GaussianBlur(decimg,(3,3),0)
             img=cv.morphologyEx(img,cv.MORPH_CLOSE,X_kernel,iterations=1)
             img=cv.morphologyEx(img,cv.MORPH_OPEN, X_kernel,iterations=1)
-            img=cv.flip(img,1)
             
             test_img=abstract(img,1,4,2)
             test_img=sizing(test_img)
-            #이미지 전처리 완료
-            #평가 predict
+            
             result=model.predict(test_img, verbose=0)
             index=np.argmax(result)
-            #결과를 전송 ex) A 만 전송
             conn.sendall(Label[index].encode())
             print(Label[index])
-            cv.imshow("SERVER", decimg)
         except Exception as e:
             _=0;
         key=cv.waitKey(50)
